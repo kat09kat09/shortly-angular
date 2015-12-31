2514c887b18c04cea8e4597f34502984f7feebb2
kat09kat09 <kat09kat09@gmail.com>
2015-12-31 13:29:44 -0500

    created shorten and links templates

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,7 +2,7 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data= Links.data;
-  $scope.data.test = [{url:'test.com'},{url:'test2.com'},{url:'test3.com'}];
+  // $scope.data.test = [{url:'test.com'},{url:'test2.com'},{url:'test3.com'}];
   $scope.getLinks= Links.getLinks; 
   $scope.getLinks()
     .then(function(data){
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,10 +2,16 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, Shorten) {
 
-  $scope.link = {};
+  // $scope.link = {};
+  //on calling addLink, pass in the link (retrieved from input box)
+
   $scope.addLink = function (link) {
+    console.log('does $scope.link exist? ', $scope.link);
+    console.log('link is: ', link); 
     return Shorten.addLink(link); 
   };
+
+
 
   // var fakeLink= {url: 'http://www.test.com'}; 
   // $scope.addLink(fakeLink)
@@ -13,3 +19,4 @@ angular.module('shortly.shorten', [])
   //     console.log('data from addlink', data); 
   //   }); 
 });
+
